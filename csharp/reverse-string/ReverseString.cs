0974a6ec80d55a94435426b8397cd26775234512
Keith Bergreen <36710853+mr-bergreen@users.noreply.github.com>
2023-07-28 12:24:08 -0400

    Update
    Completed exercise

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reversed = "";
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversed += input[i];
+        }
+
+        return reversed;
+    }
+}

389fdec30d7d04e597d810b6ef6adf6ebb4056c6
Keith Bergreen <36710853+mr-bergreen@users.noreply.github.com>
2023-10-24 19:02:23 -0400

    Completed exercises

---
+++ javascript/amusement-park/amusement-park.js
@@ -0,0 +1,77 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  var visitor = {
+    "name": name,
+    "age": age,
+    "ticketId": ticketId,
+  }
+  return visitor;
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  visitor["ticketId"] = null;
+  return visitor;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  for (let id in tickets) {
+    if (id == ticketId) {
+      if (tickets[id] == null)
+        return "not sold";
+      else
+        return "sold to " + tickets[id];
+    }
+  }
+  return "unknown ticket id";
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  var status = ticketStatus(tickets, ticketId);
+  if (status == "unknown ticket id" || status == "not sold") {
+    return "invalid ticket !!!";
+  }
+  else {
+    return status.substring(8);
+  }
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+  return visitor.gtc?.version;
+}
